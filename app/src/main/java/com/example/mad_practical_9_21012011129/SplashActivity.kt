b92f9d6efb0725fe0568d7bda8c0a35f76c5826b
student <student@gnu.ac.in>
2023-10-06 08:57:12 +0530

    .

---
+++ app/src/main/java/com/example/mad_practical_9_21012011129/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.mad_practical_9_21012011129
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.mad_practical_9_21012011129.R
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity(),Animation.AnimationListener {
+    lateinit var logoimage:ImageView
+    lateinit var logoanimation : Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        logoimage=findViewById(R.id.uvpce)
+        logoimage.setBackgroundResource((R.drawable.uvpce_logo))
+        logoanimation=AnimationUtils.loadAnimation(this,R.anim.twin_animation)
+        logoanimation.setAnimationListener(this)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+        //intent use to open animation
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
+    }
+}
